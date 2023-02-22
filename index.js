1a59f7339bbd6fd4bf448a9cb003a4ef632ce87e
Kam Lasater <kamlasater@users.noreply.github.com>
2023-02-22 09:48:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Sands');
+    res.write(JSON.stringify(process.env));
     res.end();
 }).listen(process.env.PORT || 3000);
