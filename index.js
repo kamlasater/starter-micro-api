7c4ec8c98ee3cef3e83e49f7bd8de56b7ab17e5a
Kam Lasater <kamlasater@users.noreply.github.com>
2023-03-15 11:38:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
+    console.log(`Just got a request at ${req.url} with NU_VAR=${process.env.NU_VAR}`)
     res.write(JSON.stringify(process.env));
     res.end();
 }).listen(8003);
